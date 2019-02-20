4f306ced0cb10eced746e2614be2014c2b89be74
Magaribay <mwencehe@ucsc.edu>
2019-02-20 02:02:02 -0800

    whole project uploaded

---
+++ CMPM-121-Assignment5/Assets/ScoreVar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreVar : MonoBehaviour
+{
+
+
+    public int score;
+    public Text totalScore = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalScore.text = "Jewels Collected: " + score + "/ 9";
+
+    }
+}
---
+++ CMPM-121-Assignment5/Assets/collisionDestruction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collisionDestruction : MonoBehaviour
+{
+    public GameObject destroyTrees;
+    public GameObject ScoreTextUpdate;
+    public ParticleSystem sparkle;
+    ScoreVar ScoreTextScript;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Jewel")
+        {
+            Destroy (col.gameObject);
+            ScoreTextScript.score = ScoreTextScript.score + 1;
+            print("ScoreTextScript.score");
+            sparkle.Emit(500);
+            if (ScoreTextScript.score >= 9)
+                    {
+                        Destroy(destroyTrees);
+                    }
+
+        }
+
+        if(col.gameObject.name == "BeveledStar")
+        {
+            Destroy(col.gameObject);
+            SceneManager.LoadScene("Title Screen");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreTextScript = ScoreTextUpdate.GetComponent<ScoreVar>();
+        destroyTrees = GameObject.FindGameObjectWithTag("newArea");
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
